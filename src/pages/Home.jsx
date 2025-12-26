bee281168b83d9ab5f57a0462ff332755e7ebc5f
Beslin Ebby <beslinebby00@gmail.com>
2025-12-26 21:30:41 +0530

    update scroll bar styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = ({ showSideBar }) => {
    return (
       <main className="flex h-screen w-full">
          <SideBar showSideBar={showSideBar} />
-         <div className={`pt-14 p-2 flex-1 overflow-y-scroll`}>
+         <div className="pt-14 p-2 flex-1 overflow-y-scroll home">
             <NavLinks/> 
             <Feed category={category}/>
          </div>
