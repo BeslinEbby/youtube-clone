933aa40d30b9b629da79570da8d8bd95de434eed
Beslin Ebby <beslinebby00@gmail.com>
2025-12-27 20:11:20 +0530

    add video channel details and video related buttons

--- src/components/PlayVideo.jsx
+++ src/components/PlayVideo.jsx
@@ -1,4 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { LuThumbsUp, LuThumbsDown } from "react-icons/lu";
+import { PiShareFatBold } from "react-icons/pi";
+import { FiBookmark } from "react-icons/fi";
+import { LiaDownloadSolid } from "react-icons/lia";
+import { MdMoreHoriz } from "react-icons/md";
+import ConvertValue from "../utils/ConvertValue";
+import { formatDistanceToNow } from "date-fns";
 import axiosInstance from "../api/AxiosInstance";
 
 const PlayVideo = ({ videoId }) => {
@@ -6,6 +13,7 @@ const PlayVideo = ({ videoId }) => {
    const apiKey = import.meta.env.VITE_API_KEY;
 
    const [videoData, setVideoData] = useState();
+   const [channelData, setChannelData] = useState();
 
    useEffect(() => {
       const fetchVideoData = async () => {
@@ -21,6 +29,23 @@ const PlayVideo = ({ videoId }) => {
       fetchVideoData();
    }, [videoId]);
 
+   useEffect(() => {
+      const channelId = videoData && videoData.snippet.channelId;
+
+      const fetchChannelData = async () => {
+         try {
+            const response = await axiosInstance.get(
+               `/channels?part=snippet%2Cstatistics&id=${channelId}&key=${apiKey}`
+            );
+            const data = response.data;
+            setChannelData(data && data.items[0]);
+         } catch (error) {
+            console.log("Error on fetch channel data : ", error.message);
+         }
+      };
+
+      fetchChannelData();
+   }, [videoData]);
 
    return (
       <section className="basis-[70%]">
@@ -35,7 +60,51 @@ const PlayVideo = ({ videoId }) => {
                   className="w-full h-[75vh] rounded-lg"
                ></iframe>
                <h3 className="font-bold text-xl py-3">{videoData.snippet.title}</h3>
-         </div>
+               <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-6">
+                     <div className="flex items-center gap-3 cursor-pointer">
+                        <img
+                           className="rounded-full w-10 h-10"
+                           src={channelData && channelData.snippet.thumbnails.default.url}
+                           alt="channel logo"
+                        />
+                        <div>
+                           <h4 className="font-semibold">{videoData && videoData.snippet.channelTitle}</h4>
+                           <span className="text-xs text-white/60">
+                              {ConvertValue(channelData && channelData.statistics.subscriberCount)} subscribers
+                           </span>
+                        </div>
+                     </div>
+                     <button className="text-black bg-white text-sm rounded-3xl py-2 px-4 cursor-pointer hover:bg-white/90">
+                        Subscribe
+                     </button>
+                  </div>
+
+                  <div className="flex items-center gap-4">
+                     <div className="py-2 px-3 bg-(--highlight-color) rounded-2xl flex items-center gap-1 cursor-pointer hover:bg-(--hover-color)">
+                        <LuThumbsUp className="text-xl" />
+                        <span className="pl-1 text-sm">{ConvertValue(videoData.statistics.likeCount)}</span>
+                        <hr className="text-white/60 w-6 rotate-90" />
+                        <LuThumbsDown className="text-xl" />
+                     </div>
+                     <button className="py-2 px-3 bg-(--highlight-color) rounded-3xl flex items-center gap-1 cursor-pointer hover:bg-(--hover-color)">
+                        <PiShareFatBold className="text-2xl" />
+                        <span className="text-sm">Share</span>
+                     </button>
+                     <button className="py-2 px-3 bg-(--highlight-color) rounded-3xl flex items-center gap-1 cursor-pointer hover:bg-(--hover-color)">
+                        <FiBookmark className="text-2xl" />
+                        <span className="text-sm">Save</span>
+                     </button>
+                     <button className="py-2 px-3 bg-(--highlight-color) rounded-3xl flex items-center gap-1 cursor-pointer hover:bg-(--hover-color)">
+                        <LiaDownloadSolid className="text-2xl" />
+                        <span className="text-sm">Download</span>
+                     </button>
+                     <button className="p-2 bg-(--highlight-color) rounded-full cursor-pointer hover:bg-(--hover-color)">
+                        <MdMoreHoriz className="text-2xl" />
+                     </button>
+                  </div>
+               </div>
+            </div>
          )}
       </section>
    );
